52301be04fccac4d0939bf2dc194328c1f20faa8
Pedro Ribeiro <phsony26@gmail.com>
2019-09-20 12:53:29 -0300

    ajustes

---
+++ frontend/gestacionamento/src/app/paginas/estacionamento/estacionamento.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'gestac-estacionamento',
+  templateUrl: './estacionamento.component.html',
+  styleUrls: ['./estacionamento.component.scss']
+})
+export class EstacionamentoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ frontend/gestacionamento/src/app/paginas/patio/patio.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'gestac-patio',
+  templateUrl: './patio.component.html',
+  styleUrls: ['./patio.component.scss']
+})
+export class PatioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
